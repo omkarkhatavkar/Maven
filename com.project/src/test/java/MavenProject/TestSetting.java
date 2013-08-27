fe6f1c730a6cc47f1cdaf31aeeef1e16031a4ac8
Omkar <okhatavkar007@gmail.com>
2013-08-27 08:11:05 +0530

    Testing Jenkins-Maven-Selenium Integration

--- com.project/src/test/java/MavenProject/TestSetting.java
+++ com.project/src/test/java/MavenProject/TestSetting.java
@@ -1,26 +1,61 @@
 package MavenProject;
 
+import java.util.Arrays;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 
 public class TestSetting{
 
 	public WebDriver driver;
+	static String browser;  
+
+	static {
+		browser = System.getProperty("browser");
+		System.out.println(browser);
+		
+
+
+	}
 
 	@Test(priority=1 )
 	public void StartBrowser(){
 
-		driver = new FirefoxDriver();
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			
+		}else if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",(System.getProperty("user.dir")+
+					"//chromedriver.exe"));
+
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			capabilities.setCapability("chrome.switches", Arrays.asList("--start-maximized"));
+			driver = new ChromeDriver(capabilities);
+
+		}else{
+
+			System.setProperty("webdriver.ie.driver",(System.getProperty("user.dir")+
+					"//IEDriverserver.exe"));
+
+			driver=new InternetExplorerDriver();	
+
+		}
 	}
 
 	@Test(priority=2)
 	public void NavigateURL(){
 
 		driver.get("http://www.google.com");
+		String test = driver.getTitle();
+		System.out.println(test);
 	}
-	
+
 	@Test(priority=3)
 	public void CloseBrowser(){
 		driver.close();
